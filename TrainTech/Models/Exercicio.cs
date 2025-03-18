6db30c6c3b4bc6793c4f871390bb7bbbb695e818
ArielOrtega13 <ariel.ortega13@hotmail.com>
2025-03-18 19:07:32 -0300

    Criaçao e configuração dos modelos

---
+++ TrainTech/Models/Exercicio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainTech.Models
+{
+    [Table("Exercicios")]
+    public class Exercicio
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Series { get; set; }
+        [Required]
+        public int Repetitions { get; set; }
+        [Required]
+        public double Load { get; set; } // Carga recomendada 
+        [Required]
+        public bool IsCompleted { get; set; } = false;
+    }
+}
---
+++ TrainTech/Models/Treino.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainTech.Models
+{
+    [Table("Treinos")]
+    public class Treino
+    {
+        //[BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserId { get; set; } // Identificador do usuário
+        public List<Exercicio> Exercises { get; set; } = new List<Exercicio>();
+    }
+}
+

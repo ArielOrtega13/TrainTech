d7bb0697e9d38d96f229cae91b7c76d6731091f3
ArielOrtega13 <ariel.ortega13@hotmail.com>
2025-03-18 19:33:35 -0300

    Configuração EntityFramework

---
+++ TrainTech/Migrations/20250318222632_M00.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainTech.Migrations
+{
+    public partial class M00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Treinos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treinos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exercicios",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Series = table.Column<int>(type: "int", nullable: false),
+                    Repetitions = table.Column<int>(type: "int", nullable: false),
+                    Load = table.Column<double>(type: "float", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    TreinoId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercicios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exercicios_Treinos_TreinoId",
+                        column: x => x.TreinoId,
+                        principalTable: "Treinos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercicios_TreinoId",
+                table: "Exercicios",
+                column: "TreinoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercicios");
+
+            migrationBuilder.DropTable(
+                name: "Treinos");
+        }
+    }
+}
---
+++ TrainTech/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainTech.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+               {
+        }
+        
+                
+        public DbSet<Exercicio> Exercicios { get; set; }
+        public DbSet<Treino> Treinos { get; set; }  
+    }
+}
